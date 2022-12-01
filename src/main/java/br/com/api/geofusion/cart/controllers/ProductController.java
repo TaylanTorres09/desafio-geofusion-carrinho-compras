494a5d38408f94597809af8fa74276b3e13e0374
TaylanTorres09 <taylantorres@gmail.com>
2022-11-30 21:32:55 -0300

    Create product Controller with endPoint to register Product

---
+++ src/main/java/br/com/api/geofusion/cart/controllers/ProductController.java
@@ -0,0 +1,28 @@
+package br.com.api.geofusion.cart.controllers;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.geofusion.cart.dto.ProductDto;
+import br.com.api.geofusion.cart.models.Product;
+import br.com.api.geofusion.cart.services.ProductService;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerProduct(ProductDto productDto) {
+        Product product = new Product();
+        BeanUtils.copyProperties(productDto, product);
+        return productService.registerProduct(product);
+    }
+
+}
