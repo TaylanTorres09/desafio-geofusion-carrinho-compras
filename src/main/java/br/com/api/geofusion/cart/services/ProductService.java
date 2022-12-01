4f589e4fa08a1a5ca88f92404de0815a1ce79314
TaylanTorres09 <taylantorres@gmail.com>
2022-11-30 21:29:23 -0300

    Create Service with method to register

---
+++ src/main/java/br/com/api/geofusion/cart/services/ProductService.java
@@ -0,0 +1,21 @@
+package br.com.api.geofusion.cart.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import br.com.api.geofusion.cart.models.Product;
+import br.com.api.geofusion.cart.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+    
+    @Autowired
+    private ProductRepository productRepository;
+
+    public ResponseEntity<?> registerProduct(Product product) {
+        return new ResponseEntity<Product>(productRepository.save(product), HttpStatus.CREATED);
+    }
+
+}
