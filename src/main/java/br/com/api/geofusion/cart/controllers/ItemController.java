a66f431508fb26d1ea7b9de47d9c58d918fec5d9
TaylanTorres09 <taylantorres@gmail.com>
2022-12-01 09:16:18 -0300

    Create ItemController

---
+++ src/main/java/br/com/api/geofusion/cart/controllers/ItemController.java
@@ -0,0 +1,30 @@
+package br.com.api.geofusion.cart.controllers;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.geofusion.cart.dto.ItemDto;
+import br.com.api.geofusion.cart.models.Item;
+import br.com.api.geofusion.cart.services.ItemService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+    
+    @Autowired
+    private ItemService itemService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerItem(@Valid @RequestBody ItemDto itemDto ) {
+        Item item = new Item();
+        BeanUtils.copyProperties(itemDto, item);
+        return itemService.registerItem(item);
+    }
+
+}
