3407a33aa054b8fcd0ca55b3b443b8f26a563dad
TaylanTorres09 <taylantorres@gmail.com>
2022-12-03 23:21:54 -0300

    Consert Controller Add RequestBody in method

--- src/main/java/br/com/api/geofusion/cart/controllers/ClientController.java
+++ src/main/java/br/com/api/geofusion/cart/controllers/ClientController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.geofusion.cart.dto.ClientDto;
 import br.com.api.geofusion.cart.models.Client;
 import br.com.api.geofusion.cart.services.ClientService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/client")
@@ -22,7 +24,7 @@ public class ClientController {
     private ClientService clientService;
 
     @PostMapping("/register")
-    public ResponseEntity<Client> registerClient(ClientDto clientDto) {
+    public ResponseEntity<Client> registerClient(@Valid @RequestBody ClientDto clientDto) {
         Client client = new Client();
         BeanUtils.copyProperties(clientDto, client);
         return clientService.registerClient(client);
