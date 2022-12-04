8c6e6fe2f2f22f95a3634e3c62dc64ff9203e391
TaylanTorres09 <taylantorres@gmail.com>
2022-12-04 00:39:06 -0300

    Remove

--- src/main/java/br/com/api/geofusion/cart/models/Client.java
+++ src/main/java/br/com/api/geofusion/cart/models/Client.java
@@ -1,8 +1,6 @@
 package br.com.api.geofusion.cart.models;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -11,7 +9,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
