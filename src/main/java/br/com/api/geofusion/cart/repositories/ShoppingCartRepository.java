635d6c6d02cf12323da4d27ca352b9270a749a12
TaylanTorres09 <taylantorres@gmail.com>
2022-12-04 00:09:38 -0300

    Remove desnecessary query

--- src/main/java/br/com/api/geofusion/cart/repositories/ShoppingCartRepository.java
+++ src/main/java/br/com/api/geofusion/cart/repositories/ShoppingCartRepository.java
@@ -1,7 +1,5 @@
 package br.com.api.geofusion.cart.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +7,4 @@ import br.com.api.geofusion.cart.models.ShoppingCart;
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long>{
-
-    List<ShoppingCart> findByClientId(Long clientId);
-    
 }
